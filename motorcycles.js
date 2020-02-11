1c379ceb66e25b45ca2e64332b7b39af6017119d
Doug Johnson <douglas.j.johnson@gmail.com>
2020-02-11 09:34:23 -0700

    motorcycle.html tweeking

--- motorcycles.js
+++ motorcycles.js
@@ -1,16 +1,15 @@
 let motorcycleListElement = document.getElementById('motorcycle-list')
 
-fetch("http://localhost:3000/riders/1")
+fetch(`http://localhost:3000/riders/${localStorage.getItem("rider_id")}`)
     .then(response => response.json())
     .then(rider => {
         const motorcycles = rider.motorcycle
         motorcycles.map(motorcycle => {
-            console.log(motorcycle.make)
             motorcycleElement = document.createElement('li')
             motorcycleElement.innerText = `${motorcycle.year} ${motorcycle.make} ${motorcycle.model}`
             motorcycleListElement.appendChild(motorcycleElement)
         });
     })
-//query motorcycles db stretch
 
-//
+//localStorage.setItem("rider_id","1")
+//query motorcycles db stretch
