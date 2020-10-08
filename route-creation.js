99a0fc641f39260c576ea8d84d1a3200c07e76d7
Michael Newman <coloradohokie@gmail.com>
2020-10-08 17:03:36 -0600

    add note to creation page to handle google interfacing

--- route-creation.js
+++ route-creation.js
@@ -1,4 +1,5 @@
 const URL = `https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyCZbiq-DB1UNormnDfharY7cEtLgD815FU`
+//NOTE: This key no longer works; it needs to be updated, and hidden.
 
 fetch(URL)
 .then(response => response.json())
