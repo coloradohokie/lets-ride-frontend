f546b56a6cae5fef81c72aa4067cac83fa82253c
Michael Newman <coloradohokie@gmail.com>
2020-10-21 10:28:38 -0600

    add signup page. Redirect not working

---
+++ public/js/sign-up.js
@@ -0,0 +1,45 @@
+const BASEURL = `https://lets-ride-motorcycle-app.herokuapp.com`
+
+const signUpForm = document.querySelector('.sign-up-form')
+signUpForm.addEventListener('submit', signUpUser)
+
+function signUpUser(event) {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    const username = formData.get('username')
+    const email = formData.get('email')
+    const password = formData.get('password')
+    const city = formData.get('city')
+    const state = formData.get('state')
+
+    fetch(`${BASEURL}/users`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username, email, password, city, state})
+    })
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            if (response.username) {
+                fetch(`${BASEURL}/login`,{
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({email, password})
+                })
+                .then(response => response.json())
+                .then(response => { console.log(response)
+                    
+                })
+                window.location.href = './index.html'
+            } else {
+                const failureMessage = 'Something went wrong'
+                const messageHeader = document.querySelector('.sign-up-msg')
+                messageHeader.innerText = failureMessage
+            }
+        })
+}
